8ab6f81f30fe9bb4ae7460e9aed31916b01e1896
Ask Solem <askh@opera.com>
2009-09-04 16:45:17 +0200

    Fix template

--- celeryhooks/__init__.py
+++ celeryhooks/__init__.py
@@ -1,7 +1,7 @@
 """Webhooks using Celery"""
 VERSION = (0, 0, 1)
 __version__ = ".".join(map(str, VERSION))
-__author__ = "Opera Software (WebTeam)"
-__contact__ = "portal-dev@list.opera.com"
-__homepage__ = "http://cgit.portal-dev.oslo.osa/cgit.cgi/celeryhooks/"
+__author__ = "Ask Solem"
+__contact__ = "askh@opera.com"
+__homepage__ = "http://github.com/ask/celeryhooks/"
 __docformat__ = "restructuredtext"
