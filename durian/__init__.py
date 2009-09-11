17c5b1a9c9afda7a3573f4cd606ee79b5e175b46
Ask Solem <askh@opera.com>
2009-09-11 19:21:52 +0200

    Bumped version to 0.0.2

--- durian/__init__.py
+++ durian/__init__.py
@@ -1,5 +1,5 @@
 """Webhooks for Django **EXPERIMENTAL**"""
-VERSION = (0, 0, 1)
+VERSION = (0, 0, 2)
 __version__ = ".".join(map(str, VERSION))
 __author__ = "Ask Solem"
 __contact__ = "askh@opera.com"
